741d69151f1fc76c845706cf6d71ec7c5563cdcf
mouleen <limauropro@gmail.com>
2025-03-12 17:45:03 +0000

    4G Excuse Generator 1st commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+
+function getRandomElementInArray(array) {
+  let max=array.length;
+  let position=Math.floor(Math.random() * max);
+  return array[position];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //write your code here 
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let frase=getRandomElementInArray(who) + ' ' + getRandomElementInArray(action) + ' ' + getRandomElementInArray(what) + ' ' + getRandomElementInArray(when); 
+  document.getElementById("excuse").innerHTML = frase;
 };
